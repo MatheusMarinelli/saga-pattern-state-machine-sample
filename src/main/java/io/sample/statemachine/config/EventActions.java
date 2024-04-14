327b8c78dbb79ff4a58de80b17b6f7c0edd05897
MatheusMarinelli <matheushlmarinelli@gmail.com>
2024-04-13 21:16:34 -0300

    adding Event states and actions

---
+++ src/main/java/io/sample/statemachine/config/EventActions.java
@@ -0,0 +1,8 @@
+package io.sample.statemachine.config;
+
+/**
+ * Este enum representa QUAIS ações podem acontecer para o nosso evento mudar de estado
+ */
+public enum EventActions {
+    RUN;
+}
---
+++ src/main/java/io/sample/statemachine/config/EventStates.java
@@ -0,0 +1,13 @@
+package io.sample.statemachine.config;
+
+/**
+ * Este enum representa os possíveis estados de um processo/transação
+ */
+public enum EventStates {
+
+    INITIAL,
+    EXECUTION,
+    FINISHED,
+    ERROR;
+
+}
